d9d3d45c0b07e258c80c0433c43a3d76fcf2a9c2
zahiri2020 <150599684+zahiri2020@users.noreply.github.com>
2024-04-30 10:55:42 +0100

    17

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 02:29:30 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/04/30 10:38:44 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/04/30 10:55:26 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		len;
 
 	if (!s1)
-		return (NULL);
+		return (ft_strdup(s2));
 	len = ft_strlen(s1) + ft_strlen(s2);
 	str = (char *)malloc (sizeof(char) * (len + 1));
 	if (!str)
